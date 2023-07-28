23ade75f1fcd767dc1b0b855376055cc81812ff7
Sean Norwood <norwood.sean@gmail.com>
2023-07-27 23:04:35 -0500

    green: more tests for calculate age

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -6,4 +6,22 @@ describe("AgeCalculator", () => {
 
     expect(calc.calculateAge("Mercury")).toBe(150);
   });
+
+  test("calculate age in venus years", () => {
+    const calc = new AgeCalculator(36);
+
+    expect(calc.calculateAge("Venus")).toBeCloseTo(58.06);
+  });
+
+  test("calculate age in Mars years", () => {
+    const calc = new AgeCalculator(36);
+
+    expect(calc.calculateAge("Mars")).toBeCloseTo(19.148);
+  });
+
+  test("calculate age in Jupiter years", () => {
+    const calc = new AgeCalculator(36);
+
+    expect(calc.calculateAge("Jupiter")).toBeCloseTo(3.035);
+  });
 });
